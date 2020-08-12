070770d5cc6ce4ba2099fd1a5e2ab5c240ad514f
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-08-12 01:07:23 -0700

    added margin above cards for proper image spacing

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,11 +1,14 @@
 import React from "react"
+import "./style.css"
 
 function Image(props) {
     return (
-        <div className="col m4">
-            <div className="card" id={props.id}>
-                <div className="card-image">
-                    <img onClick={props.onClick} src={require(`${props.url}`)} alt="placeholder"/>
+        <div className="imgContainer">
+            <div className="col m4 s3 l6">
+                <div className="card" id={props.id}>
+                    <div className="card-image">
+                        <img onClick={props.onClick} src={require(`${props.url}`)} alt="placeholder" />
+                    </div>
                 </div>
             </div>
         </div>
